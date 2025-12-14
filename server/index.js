fc1629693a41afc037a3d807495f6ef410ab4a94
Sharath Chandra <sharathchandraprodduturi01@gmail.com>
2025-12-14 12:56:20 +0530

    Fix Email Errors for production

--- server/index.js
+++ server/index.js
@@ -51,8 +51,8 @@ const contactLimiter = rateLimit({
  */
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS.replace(/\s+/g, '')
